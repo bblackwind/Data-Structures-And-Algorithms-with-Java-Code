05e44e5e15a6ec5100f623c5bdeaed2ac9e4e403
bblackwind <vishal230396@gmail.com>
2025-09-21 02:00:19 +0530

    printDigitsOfNumber

--- Getting Started/l001.java
+++ Getting Started/l001.java
@@ -4,6 +4,7 @@ public class l001{
 
     public static Scanner sc = new Scanner(System.in);
 
+// function to count number of digits
     public static int countNoOfDigit(int n){
         int len = 0;
         while(n>=0){
@@ -13,8 +14,45 @@ public class l001{
         return len;
     }
 
+//function to print number in Reverse order
+    public static void printDigitsInReverseOrder(int n){
+        while(n>0){
+            int digit = n%10;
+            n/=10;
+            System.out.println(digit);
+        }
+    }
+
+//power function
+    public static int powerEqualDigits(int n){
+        int pow = 1;
+        while(n!=0){
+            pow *= 10;
+            n /= 10;
+        }
+        return pow;
+    }
+
+//function to print numbers in forward order
+
+    public static void printDigitsOfNumber(int n){
+
+        int pow = powerEqualDigits(n);
+        pow /= 10;
+        while(pow != 0){
+            int quo = n/pow;
+            n %= pow;
+            pow /= 10;
+            System.out.println(quo);
+        }
+
+    }
+
+
     public static void main(String[] args){
-        int n = 95820;  
-        System.out.println(countNoOfDigit(n));
+        int n = 9582000;  
+        // System.out.println(countNoOfDigit(n));
+        // printDigitsInReverseOrder(n);
+        printDigitsOfNumber(n);
     }
 }

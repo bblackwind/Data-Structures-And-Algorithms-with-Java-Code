13a0bea2824d8b791a47a47a2169e35224b24fd7
bblackwind <vishal230396@gmail.com>
2025-09-28 19:22:17 +0530

    printSquare

---
+++ Getting Started/2.Patterns/pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class pattern{
+
+//cst => count of stars
+//nst => no. of stars
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void printSquare(int n){
+        int nst = n;
+        for(int row = 1; row <=n ; row++){
+            for(int cst = 1; cst<=nst ; cst++){
+                System.out.print("*");
+            }
+            nst = n;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        // System.out.println("hello ji");
+        int n = sc.nextInt();
+        printSquare(n);
+    }
+}

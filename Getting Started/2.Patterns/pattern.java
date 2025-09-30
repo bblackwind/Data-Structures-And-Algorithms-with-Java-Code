51d9726982390479d76c96238230106662ac4342
bblackwind <vishal230396@gmail.com>
2025-09-30 17:46:34 +0530

    Full Diamond

--- Getting Started/2.Patterns/pattern.java
+++ Getting Started/2.Patterns/pattern.java
@@ -80,6 +80,29 @@ public class pattern{
         }
     }
 
+    public static void fullDiamond(int n){
+        int nst = 1;
+        int nsp = n/2;
+
+        for(int row = 1; row <= n ; row++){
+            for(int csp=1 ; csp<=nsp ; csp++){
+                System.out.print(" ");
+            }
+             for(int cst=1;cst<=nst;cst++){
+                System.out.print("*");
+            }
+
+            if(row <= n/2){
+                nst += 2;
+                nsp--;
+            }else{
+                nst -= 2;
+                nsp++;
+            }
+            System.out.println();
+        }
+    }
+
     
 
     public static void main(String[] args){

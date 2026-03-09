3abe95753e76bd2dee98ef44d82ce562672fc360
bblackwind <vishal230396@gmail.com>
2026-03-09 15:05:30 +0530

    digitOfNumber

--- Getting Started/1.Started_1/l001.java
+++ Getting Started/1.Started_1/l001.java
@@ -18,8 +18,34 @@ public class l001{
         }
     }
 
+    public static int powerEqualDigit(int n){
+        int pow = 1;
+        while(n != 0){
+            pow *= 10;
+            n /= 10;
+        }
+        return pow;
+    }
+
+    public static void digitOfNumber(int n){
+        int pow = powerEqualDigit(n);
+        pow /= 10;
+
+        while(pow > 0){
+            int digit = n / pow;
+            System.out.println(digit);
+            n %= pow;
+            pow /= 10;
+        }
+    }
+
+
+
     public static void main(String[] args){
         // System.out.println(countDigits(111));
-        printDigitsInReverseOrder(123485278);
+        // printDigitsInReverseOrder(123485278);
+        // System.out.println(powerEqualDigit(123485278));
+        digitOfNumber(123485278);
+        
     }
 }

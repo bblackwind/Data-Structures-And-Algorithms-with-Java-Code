5f76130f9992e2d151a806a882251a1a686676dc
bblackwind <vishal230396@gmail.com>
2026-05-29 08:49:24 +0530

    code update

--- Getting Started/3.Arrays/l001.java
+++ Getting Started/3.Arrays/l001.java
@@ -155,7 +155,7 @@ public class l001{
 
        int i = n-1, j = m-1, k=p-1, carry=0;
        while(k>=0){
-        int sum = carry + (i>=0?arr1[i]:0)+(j>=0?arr2[j]:0);
+        int sum = carry + (i >= 0 ? arr1[i] : 0)+(j >= 0 ? arr2[j] : 0);
         ans[k] = sum%10;
         carry = sum/10;
 
